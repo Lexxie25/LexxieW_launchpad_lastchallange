108643772a468323bb156b89f343e5ea7406833e
Lexxie25 <lexxieworth@gmail.com>
2022-08-29 19:10:27 -0600

    Iteration

---
+++ QuestionClass/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public static class QuestionClass
+{
+    static void Main()
+    {
+        Names();
+
+    }
+    static List<string> NamesList = new List<string>()
+ {
+ "Jimmy",
+ "Jeffrey",
+ "John",
+ };
+
+    public static void Names()
+    {
+        string names = string.Join(", ", NamesList);
+        Console.WriteLine(names);
+    }
+
+}
+
+
+
+
+//Iterate through namesList with   public static void Names()
+
+//this does Write out the names when i used a different console app cant get to pop up terminal because of error with main program 
